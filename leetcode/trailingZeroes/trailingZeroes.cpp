b6ca808ddb385a0222cc94778fa5bcdda1020db4
239852590@qq.com <239852590@qq.com>
2020-03-07 23:43:34 +0800

    find the number of the given number factorial

---
+++ leetcode/trailingZeroes/trailingZeroes.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        int cnt=0;
+        while(n)
+        {
+            n/=5;
+            cnt+=n;
+        }
+        return cnt;
+    }
+};
