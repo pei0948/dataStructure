0155f425e029219caeea9350f575a12565c9033e
239852590@qq.com <239852590@qq.com>
2020-03-24 23:14:39 +0800

    consider negative num

--- leetcode/hammingWeight/hammingWeight.cpp
+++ leetcode/hammingWeight/hammingWeight.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    int hammingWeight(uint32_t n) {
+    int hammingWeight1(uint32_t n) //复数情况不适用
+	{
         int cnt=0;
         while(n)
         {
@@ -9,6 +10,16 @@ public:
                 ++cnt;
             }
             n>>=1;
+        }
+        return cnt;
+    }
+	int hammingWeight2(uint32_t n) //也适用于复数
+	{
+        int cnt=0;
+        while(n)
+        {
+            ++cnt;
+            n=(n-1)&n;
         }
         return cnt;
     }
