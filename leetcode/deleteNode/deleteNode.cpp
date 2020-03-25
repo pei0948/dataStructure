50ae3edea106f268ae8f946a22fe180d7047e7f4
239852590@qq.com <239852590@qq.com>
2020-03-25 23:55:37 +0800

    delete the node in the given list

--- leetcode/deleteNode/deleteNode.cpp
+++ leetcode/deleteNode/deleteNode.cpp
@@ -12,4 +12,35 @@ public:
         node->val=node->next->val;
         node->next=node->next->next;
     }
+	
+	ListNode* deleteNode(ListNode* head, int val) {
+        ListNode *curr=head,
+                    *pre=nullptr;
+        while(curr!=nullptr && curr->val!=val)
+        {
+            pre=curr;
+            curr=curr->next;
+        }
+        if(curr==nullptr)
+        {
+            return head;
+        }
+        if(curr->next==nullptr)
+        {
+            if(pre==nullptr)
+            {
+                return nullptr;
+            }
+            else
+            {
+                pre->next=nullptr;
+            }
+        }
+        else
+        {
+            curr->val=curr->next->val;
+            curr->next=curr->next->next;
+        }
+        return head;
+    }
 };
